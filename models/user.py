d1fbf85e2927a1d60775eb302f89f186e9a95b40
Daniele Ding <dinghino@gmail.com>
2017-03-22 10:50:51 +0100

    add BaseModel

--- models/user.py
+++ models/user.py
@@ -1,3 +1,20 @@
 """
 Models contains the sqlite3 database models for the application.
 """
+
+from peewee import SqliteDatabase
+from peewee import Model
+from peewee import CharField
+from peewee import UUIDField
+
+
+database = SqliteDatabase('database.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+class User(BaseModel):
+    pass
