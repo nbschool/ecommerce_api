cc75a0bdcf8f55d5fed0b9a093d0fcd2ffd947e2
Eugenio Minissale <eugeniominissale@gmail.com>
2017-03-27 18:36:46 +0200

    cleaned test_orders.py

--- test_orders.py
+++ test_orders.py
@@ -1,23 +1,12 @@
 from app import app
-from app import Order
-from app import Item
-from app import OrderItem
-
-from models import database
-from models import create_tables
-from models import DateTimeEncoder
-from models import populate_tables
-
-from http.client import CREATED
-from http.client import NO_CONTENT
-from http.client import NOT_FOUND
-from http.client import OK
-from http.client import BAD_REQUEST
+from models import  Order, OrderItem, Item, database, create_tables
+from http.client import CREATED, NO_CONTENT, NOT_FOUND, OK, INTERNAL_SERVER_ERROR
 from peewee import SqliteDatabase
-
 import json
 import uuid
 import datetime
+import sys
+sys.path.append('./views')
 
 class TestOrders:
 	@classmethod
@@ -49,10 +38,10 @@ class TestOrders:
 			price = "20.00",
 			description = "item1description."
 		)
-		oid =  uuid.uuid4()
-		dt = json.dumps(datetime.datetime.now(),cls=DateTimeEncoder)
+		order_id =  uuid.uuid4()
+		dt = datetime.datetime.now().isoformat()
 		order1 = Order.create(
-			order_id = oid,
+			order_id = order_id,
 			date = dt,
 			total_price = 100,
 			delivery_address = 'Via Rossi 12'
@@ -65,7 +54,7 @@ class TestOrders:
 		)
 		resp = self.app.get('/orders/')
 		assert resp.status_code == OK
-		assert json.loads(resp.data) == [{"order_id": str(oid), "date": dt, "total_price": 100.0, "delivery_address": 'Via Rossi 12', "items": [{"quantity": 2, "subtotal": 50.0, "item_name": "item1", "item_description": "item1description."}]}]
+		assert json.loads(resp.data) == [{"order_id": str(order_id), "date": dt, "total_price": 100.0, "delivery_address": 'Via Rossi 12', "items": [{"quantity": 2, "subtotal": 50.0, "item_name": "item1", "item_description": "item1description."}]}]
 
 	def test_get_order__non_existing_empty_orders(self):
 		resp = self.app.get('/orders/{}©√'.format(uuid.uuid4()))
@@ -80,7 +69,7 @@ class TestOrders:
 		)
 		order1 = Order.create(
 			order_id = uuid.uuid4(),
-			date = json.dumps(datetime.datetime.now(),cls=DateTimeEncoder),
+			date = datetime.datetime.now().isoformat(),
 			total_price = 100,
 			delivery_address = 'Via Rossi 12'
 		)
@@ -101,10 +90,10 @@ class TestOrders:
 			description = "item1description."
 		)
 
-		oid = uuid.uuid4()
-		dt = json.dumps(datetime.datetime.now(), cls=DateTimeEncoder)
+		order_id = uuid.uuid4()
+		dt = datetime.datetime.now().isoformat()
 		order1 = Order.create(
-			order_id = oid,
+			order_id = order_id,
 			date = dt,
 			total_price = 100,
 			delivery_address = 'Via Rossi 12'
@@ -123,7 +112,7 @@ class TestOrders:
 		)
 		order2 = Order.create(
 			order_id = uuid.uuid4(),
-			date = json.dumps(datetime.datetime.now(), cls=DateTimeEncoder),
+			date = datetime.datetime.now().isoformat(),
 			total_price = 200,
 			delivery_address = 'Via Verdi 12'
 		)
@@ -134,9 +123,9 @@ class TestOrders:
 			subtotal = 100.00
 		)
 
-		resp = self.app.get('/orders/{}'.format(oid))
+		resp = self.app.get('/orders/{}'.format(order_id))
 		assert resp.status_code == OK
-		assert json.loads(resp.data) == [str(oid), dt, 100.0, 'Via Rossi 12', [{"quantity": 2, "subtotal": 50.0, "item_name": "item1", "item_description": "item1description."}]]
+		assert json.loads(resp.data) == [str(order_id), dt, 100.0, 'Via Rossi 12', [{"quantity": 2, "subtotal": 50.0, "item_name": "item1", "item_description": "item1description."}]]
 
 	def test_delete_article__success(self):
 		item1 = Item.create(
@@ -145,10 +134,10 @@ class TestOrders:
 			price = "20.00",
 			description = "item1description."
 		)
-		oid =  uuid.uuid4()
-		dt = json.dumps(datetime.datetime.now(), cls=DateTimeEncoder)
+		order_id =  uuid.uuid4()
+		dt = datetime.datetime.now().isoformat()
 		order1 = Order.create(
-			order_id = oid,
+			order_id = order_id,
 			date = dt,
 			total_price = 100,
 			delivery_address = 'Via Rossi 12'
@@ -161,11 +150,11 @@ class TestOrders:
 		)
 		order2 = Order.create(
 			order_id = uuid.uuid4(),
-			date = json.dumps(datetime.datetime.now(), cls=DateTimeEncoder),
+			date = datetime.datetime.now().isoformat(),
 			total_price = 200,
 			delivery_address = 'Via Verdi 12'
 		)
-		resp = self.app.delete('/orders/{}'.format(str(oid)))
+		resp = self.app.delete('/orders/{}'.format(str(order_id)))
 		assert resp.status_code == NO_CONTENT
 		assert len(Order.select()) == 1
 		assert len(OrderItem.select()) == 0
@@ -178,19 +167,10 @@ class TestOrders:
 	def test_delete_order__failure__failure_non_existing(self):
 		order1 = Order.create(
 			order_id = uuid.uuid4(),
-			date = json.dumps(datetime.datetime.now(), cls=DateTimeEncoder),
+			date = datetime.datetime.now().isoformat(),
 			total_price = 100,
 			delivery_address = 'Via Rossi 12'
 		)
 		resp = self.app.delete('/orders/{}'.format(str(uuid.uuid4())))		
 		assert resp.status_code == NOT_FOUND
 		assert len(Order.select()) == 1
-
-	def test_db_functionality(self):
-		populate_tables()
-		# Inner join between the three tables
-		res = (Order
-			.select()
-			.join(OrderItem)
-			.join(Item))
-		print([o.json() for o in res])
