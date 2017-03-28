5a79537f58e761b4a0ddcb5a22225946b4ea2c69
Marco Tinacci <marco.tinacci@gmail.com>
2017-03-28 15:55:18 +0200

    clean style

--- tests/test_items.py
+++ tests/test_items.py
@@ -5,8 +5,7 @@ Test suite for ItemHandler and ItemListHandler
 import json
 
 from app import app
-from models import Item as ItemModel
-from models import connect, close
+from models import Item
 
 from peewee import SqliteDatabase
 import http.client as client
@@ -36,22 +35,18 @@ TEST_ITEM_PRECISION = {
 class TestItems:
     @classmethod
     def setup_class(cls):
-        ItemModel._meta.database = SqliteDatabase(':memory:')
-        connect()
+        Item._meta.database = SqliteDatabase(':memory:')
+        Item.create_table(fail_silently=True)
         cls.app = app.test_client()
 
-    @classmethod
-    def teardown_class(cls):
-        close()
-
     def setup_method(self):
-        ItemModel.delete().execute()
+        Item.delete().execute()
 
     def test_post_item__success(self):
         resp = self.app.post('/items/', data=TEST_ITEM)
         assert resp.status_code == client.CREATED
-        assert len(ItemModel.select()) == 1
-        assert ItemModel.select()[0].json() == TEST_ITEM
+        assert len(Item.select()) == 1
+        assert Item.select()[0].json() == TEST_ITEM
 
     def test_post_item__failed(self):
         resp = self.app.post('/items/', data=TEST_ITEM_WRONG)
@@ -60,15 +55,15 @@ class TestItems:
     def test_post_item__round_price(self):
         resp = self.app.post('/items/', data=TEST_ITEM_PRECISION)
         assert resp.status_code == client.CREATED
-        item = ItemModel.select().get()
+        item = Item.select().get()
         assert round(TEST_ITEM_PRECISION['price'], 2) == float(item.price)
         assert not TEST_ITEM_PRECISION['price'] == item.price
         assert TEST_ITEM_PRECISION['name'] == item.name
         assert TEST_ITEM_PRECISION['description'] == item.description
 
     def test_get_items__success(self):
-        ItemModel.create(**TEST_ITEM)
-        ItemModel.create(**TEST_ITEM2)
+        Item.create(**TEST_ITEM)
+        Item.create(**TEST_ITEM2)
         resp = self.app.get('/items/')
         items = json.loads(resp.data)
         assert resp.status_code == client.OK
@@ -77,7 +72,7 @@ class TestItems:
         assert TEST_ITEM2 in items
 
     def test_get_item__success(self):
-        item = ItemModel.create(**TEST_ITEM)
+        item = Item.create(**TEST_ITEM)
         resp = self.app.get('/items/{item_id}'.format(item_id=item.id))
         assert resp.status_code == client.OK
         assert json.loads(resp.data) == TEST_ITEM
@@ -87,11 +82,11 @@ class TestItems:
         assert resp.status_code == client.NOT_FOUND
 
     def test_put_item__success(self):
-        item = ItemModel.create(**TEST_ITEM)
+        item = Item.create(**TEST_ITEM)
         resp = self.app.put('/items/{item_id}'.format(item_id=item.id),
                             data=TEST_ITEM2)
         assert resp.status_code == client.OK
-        db_item = ItemModel.select().where(ItemModel.id == item.id).get()
+        db_item = Item.select().where(Item.id == item.id).get()
         assert db_item.json() == TEST_ITEM2
 
     def test_put_item__failed(self):
@@ -100,10 +95,10 @@ class TestItems:
         assert resp.status_code == client.NOT_FOUND
 
     def test_delete_item__success(self):
-        item = ItemModel.create(**TEST_ITEM)
+        item = Item.create(**TEST_ITEM)
         resp = self.app.delete('/items/{item_id}'.format(item_id=item.id))
         assert resp.status_code == client.NO_CONTENT
-        assert not ItemModel.select().exists()
+        assert not Item.select().exists()
 
     def test_delete_item__failed(self):
         resp = self.app.delete('/items/{item_id}'.format(item_id=1))
