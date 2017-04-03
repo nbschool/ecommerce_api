faa702d59934681f1b2b256b2ac7e3a2604cee17
Alessandro Brugioni <info@toysroom.it>
2017-04-03 12:48:20 +0200

    docstrings

--- schemas.py
+++ schemas.py
@@ -6,24 +6,36 @@ from jsonschema.exceptions import ValidationError
 
 
 class BaseSchema(Schema):
+    """
+    Base class for all Schemas.
+    Extends marshmallow.Schema and Implements utility functions to validate
+    inputs for the classes, to get schemas and serialize models data.
+    """
     @classmethod
     def json(cls, obj):
         """
-        converte un oggetto complesso (User, ad esempio) in un oggetto serializzabile (marshmallow object), un json in pratica
+        http://marshmallow.readthedocs.io/en/latest/quickstart.html#serializing-objects-dumping
+        Serialize obj by passing it to schema's dump method,
+        which returns the formatted result
         """
         return cls().dump(obj).data
 
     @classmethod
     def json_schema(cls):
         """"
-        converte un marshmallow schemas in un JSON Schema
+        convert a marhsmallow object to a json
         """
         return JSONSchema().dump(cls()).data
 
     @classmethod
     def validate_input(cls, jsondata):
         """"
-        valida il json con il suo schema
+        http://marshmallow.readthedocs.io/en/latest/quickstart.html#schema-validate
+        validate an input json data against the schema of its relative object
+        schema
+        :returns:
+        * True if validation is ok
+        * Errors dict {<key>: <error>} if validation does not pass
         """
         errors = cls().validate(jsondata)
         if not errors:
@@ -33,6 +45,9 @@ class BaseSchema(Schema):
 
 
 class UserSchema(BaseSchema):
+    """
+    Schema for models.User
+    """
     first_name = fields.Str(required=True)
     last_name = fields.Str(required=True)
     email = fields.Email(required=True)
