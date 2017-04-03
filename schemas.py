e40f854d1a9d6d766b16156031be773d063576b7
Alessandro Brugioni <info@toysroom.it>
2017-04-03 13:27:05 +0200

    fix load_only for password
    typo

--- schemas.py
+++ schemas.py
@@ -51,4 +51,4 @@ class UserSchema(BaseSchema):
     first_name = fields.Str(required=True)
     last_name = fields.Str(required=True)
     email = fields.Email(required=True)
-    password = fields.Str(required=True, loads_only=True)
+    password = fields.Str(required=True, load_only=True)
