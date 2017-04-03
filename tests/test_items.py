b95ad460cc1f5f1185c75fb1c2dcf6faf6ce7431
Francesco Mirabelli <ceskomira90@gmail.com>
2017-04-03 10:53:04 +0200

    tests updated to the new item model

--- tests/test_items.py
+++ tests/test_items.py
@@ -3,6 +3,7 @@ Test suite for ItemHandler and ItemListHandler
 """
 
 import json
+import uuid
 
 from peewee import SqliteDatabase
 import http.client as client
@@ -11,25 +12,30 @@ from app import app
 from models import Item
 
 TEST_ITEM = {
+    'item_id': '429994bf-784e-47cc-a823-e0c394b823e8',
     'name': 'mario',
     'price': 20.20,
     'description': 'svariati mariii'
 }
 TEST_ITEM2 = {
+    'item_id': '577ad826-a79d-41e9-a5b2-7955bcf03499',
     'name': 'GINO',
     'price': 30.20,
     'description': 'svariati GINIIIII'
 }
 TEST_ITEM_WRONG = {
+    'id': '19b4c6dc-e393-4e76-bf0f-72559dd5d32e',
     'name': '',
     'price': 30.20,
     'description': 'svariati GINIIIII'
 }
 TEST_ITEM_PRECISION = {
+    'id': '68e587f7-3982-4b6a-a882-dd43b89134fe',
     'name': 'Anna Pannocchia',
     'price': 30.222222,
     'description': 'lorem ipsum'
 }
+WRONG_UUID = '04f2f213-1a0f-443d-a5ab-79097ba725ba'
 
 
 class TestItems:
@@ -47,8 +53,14 @@ class TestItems:
                              content_type='application/json')
         assert resp.status_code == client.CREATED
         assert len(Item.select()) == 1
-        assert Item.select()[0].json() == TEST_ITEM
-        assert Item.select()[0].id == json.loads(resp.data)['item_id']
+        item = Item.select()[0].json()
+        assert item['name'] == TEST_ITEM['name']
+        assert item['price'] == TEST_ITEM['price']
+        assert item['description'] == TEST_ITEM['description']
+        try:
+            uuid.UUID(item['item_id'], version=4)
+        except ValueError:
+            assert False
 
     def test_post_item__failed(self):
         resp = self.app.post('/items/', data=json.dumps(TEST_ITEM_WRONG),
@@ -77,41 +89,45 @@ class TestItems:
 
     def test_get_item__success(self):
         item = Item.create(**TEST_ITEM)
-        resp = self.app.get('/items/{item_id}'.format(item_id=item.id))
+        resp = self.app.get('/items/{item_id}'.format(item_id=item.item_id))
         assert resp.status_code == client.OK
         assert json.loads(resp.data) == TEST_ITEM
 
     def test_get_item__failed(self):
-        resp = self.app.get('/items/{item_id}'.format(item_id=1))
+        resp = self.app.get('/items/{item_id}'.format(item_id=WRONG_UUID)) #todo
         assert resp.status_code == client.NOT_FOUND
 
     def test_put_item__success(self):
         item = Item.create(**TEST_ITEM)
-        resp = self.app.put('/items/{item_id}'.format(item_id=item.id),
+        resp = self.app.put('/items/{item_id}'.format(item_id=item.item_id),
                             data=json.dumps(TEST_ITEM2),
                             content_type='application/json')
         assert resp.status_code == client.OK
-        db_item = Item.select().where(Item.id == item.id).get()
-        assert db_item.json() == TEST_ITEM2
+        json_item = Item.select().where(Item.item_id == item.item_id).get().json()
+        assert json_item['name'] == TEST_ITEM2['name']
+        assert json_item['price'] == TEST_ITEM2['price']
+        assert json_item['description'] == TEST_ITEM2['description']
+        assert json_item['item_id'] == item.item_id
 
     def test_put_item__wrong_id(self):
-        resp = self.app.put('/items/{item_id}'.format(item_id=1),
+        item = Item.create(**TEST_ITEM)
+        resp = self.app.put('/items/{item_id}'.format(item_id=WRONG_UUID),
                             data=json.dumps(TEST_ITEM2),
                             content_type='application/json')
         assert resp.status_code == client.NOT_FOUND
 
     def test_put_item__failed(self):
-        resp = self.app.put('/items/{item_id}'.format(item_id=1),
+        resp = self.app.put('/items/{item_id}'.format(item_id=WRONG_UUID),
                             data=json.dumps(TEST_ITEM),
                             content_type='application/json')
         assert resp.status_code == client.NOT_FOUND
 
     def test_delete_item__success(self):
-        item = Item.create(**TEST_ITEM)
-        resp = self.app.delete('/items/{item_id}'.format(item_id=item.id))
+        item = Item.create(**TEST_ITEM2)
+        resp = self.app.delete('/items/{item_id}'.format(item_id=item.item_id))
         assert resp.status_code == client.NO_CONTENT
         assert not Item.select().exists()
 
     def test_delete_item__failed(self):
-        resp = self.app.delete('/items/{item_id}'.format(item_id=1))
+        resp = self.app.delete('/items/{item_id}'.format(item_id=WRONG_UUID))
         assert resp.status_code == client.NOT_FOUND
