83006154f0aecd10d27e505b693f6b05d410aff9
Daniele Ding <dinghino@gmail.com>
2017-04-04 11:55:35 +0200

    add user_id to User.json()

--- models.py
+++ models.py
@@ -96,6 +96,7 @@ class User(BaseModel):
         """
 
         return {
+            'user_id': str(self.user_id),
             'first_name': self.first_name,
             'last_name': self.last_name,
             'email': self.email
