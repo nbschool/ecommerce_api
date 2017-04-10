8e279211f43c6b285eb1ec26a5642176de229767
Eugenio Minissale <eugeniominissale@gmail.com>
2017-04-10 11:21:55 +0200

    added test address

---
+++ tests/test_addresses.py
@@ -0,0 +1,55 @@
+import json
+import uuid
+
+from peewee import SqliteDatabase
+import http.client as client
+
+from app import app
+from http.client import CREATED, NO_CONTENT, NOT_FOUND, OK, BAD_REQUEST
+from models import Item, Address, User
+from tests.test_utils import add_user, open_with_auth
+
+TEST_USER_PSW = '123'
+
+def get_test_addr_dict(user, country='Italy', city='Pistoia', post_code='51100',
+                       address='Via Verdi 12', phone='3294882773'):
+    return {
+        'address_id': uuid.uuid4(),
+        'user': user,
+        'user_first_name': user.first_name,
+        'user_last_name': user.last_name,
+        'country': country,
+        'city': city,
+        'post_code': post_code,
+        'address': address,
+        'phone': phone
+    }
+
+
+class TestAddresses:
+    @classmethod
+    def setup_class(cls):
+        test_db = SqliteDatabase(':memory:')
+        Address._meta.database = test_db
+        User._meta.database = test_db
+        test_db.connect()
+        Address.create_table()
+        User.create_table()
+        cls.app = app.test_client()
+
+    def setup_method(self):
+        Address.delete().execute()
+        User.delete().execute()
+
+    def test_get_addresses__empty(self):
+
+        user = add_user('mariorossi@gmail.com', TEST_USER_PSW)
+
+        resp = open_with_auth(self.app, '/addresses/', 'GET',
+                             user.email, TEST_USER_PSW, None,
+                             None)
+
+        resp = self.app.get('/addresses/')
+        import pdb; pdb.set_trace()
+        assert resp.status_code == OK
+        assert json.loads(resp.data) == []
