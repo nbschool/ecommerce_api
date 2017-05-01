d0bd068a3f8cf3bf9488c3cdb4c445a6dd642887
Daniele Ding <dinghino@gmail.com>
2017-05-01 14:29:05 +0200

    fix test_utils.add_user to dump example user dicts
    allow unpacking of dicts representing users directly to add_item

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -20,7 +20,8 @@ def wrong_dump(data):
         "{}={}".format(k, v) for k, v in zip(data.keys(), data.values())])
 
 
-def add_user(email, psw, id=None):
+
+def add_user(email, password, id=None, first_name='John', last_name='Doe'):
     """
     Create a single user in the test database.
     If an email is provided it will be used, otherwise it will be generated
@@ -29,10 +30,10 @@ def add_user(email, psw, id=None):
     email = email or 'johndoe{}@email.com'.format(int(random.random() * 100))
 
     return User.create(
-        first_name='John',
-        last_name='Doe',
+        first_name=first_name,
+        last_name=last_name,
         email=email,
-        password=User.hash_password(psw),
+        password=User.hash_password(password),
         uuid=id or uuid4(),
     )
 
