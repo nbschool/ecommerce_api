523b1c1e7d738e53413bd703e9f8ee649d7cf3c7
German Lugo <bitnibs@gmail.com>
2017-05-08 10:54:05 +0200

    create GET and POST.

--- utils.py
+++ utils.py
@@ -30,7 +30,7 @@ def check_required_fields(required_fields, request_data):
 def non_empty_str(val, name):
     """ Custom type for reqparser, blocking empty strings """
     if not str(val).strip():
-        raise ValueError('The argument {} is not empty'.format(name))
+        raise ValueError('The argument {} cannot be empty'.format(name))
     return str(val)
 
 
