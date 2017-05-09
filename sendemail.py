d984b4e58e7f695eac23d2374c714e6070071c83
Francesco Mirabelli <ceskomira90@gmail.com>
2017-05-09 11:42:03 +0200

    added function sendemail that send email with mailgun

---
+++ sendemail.py
@@ -0,0 +1,31 @@
+import requests
+import os
+
+KEY = os.getenv('MAILGUN_API_KEY')
+SANDBOX = os.getenv('MAILGUN_DOMAIN')
+ENVIRONMENT = os.getenv('ENVIRONMENT')
+
+def send_email(subject, body):
+
+    if ENVIRONMENT == 'dev':
+        print('Status: 200')
+        print('Body: Trying send email')
+        return
+
+    elif ENVIRONMENT == 'staging':
+        email = os.getenv('ADMIN_MAIL_STAGING')
+        recipient = os.getenv('NOTIFICATION_MAIL_STAGING')
+
+    else:
+        email = os.getenv('ADMIN_MAIL')
+        recipient = os.getenv('NOTIFICATION_MAIL')
+
+    request_url = 'https://api.mailgun.net/v2/{0}/messages'.format(SANDBOX)
+    request = requests.post(request_url, auth=('api', KEY), data={
+        'from': email,
+        'to': recipient,
+        'subject': subject,
+        'text': body
+    })
+    return request
+
