49a8496df02fd23af0d609f42af99a43213ab43c
German Lugo <bitnibs@gmail.com>
2017-05-10 16:36:43 +0200

    Fix all bugs.

--- tests/test_favorites.py
+++ tests/test_favorites.py
@@ -13,16 +13,16 @@ class TestFavorites(TestCase):
 
     def test_get_favorites__empty(self):
         user = add_user(USER1, PASS1)
-        user_path = 'favorite/'
+        user_path = 'favorites/'
+        EXPECTED_MSG = b'[{"Message": "Sorry, You haven\'t select any favorite item yet."}]\n'
         resp = open_with_auth(self.app, API_ENDPOINT.format(user_path), 'GET',
                       user.email, PASS1, None, None)
-        # import pdb; pdb.set_trace()
         assert resp.status_code == NOT_FOUND
-        # assert resp.data == []
+        assert resp.data == EXPECTED_MSG
 
     def test_get_favorites_pass__wrong(self):
         user = add_user(USER1, PASS1)
-        user_path = 'favorite/'
+        user_path = 'favorites/'
         resp = open_with_auth(self.app, API_ENDPOINT.format(user_path), 'GET',
                       user.email, PASS2, None, None)
         assert resp.status_code == UNAUTHORIZED
