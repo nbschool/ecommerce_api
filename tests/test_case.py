8987bf57e7cf6fc8c9b1c250264d2775a3116cd5
German Lugo <bitnibs@gmail.com>
2017-05-10 11:10:19 +0200

    Add Favorite in test_case.py.

--- tests/test_case.py
+++ tests/test_case.py
@@ -3,10 +3,10 @@ Test case for setup and teardown methods
 """
 
 from app import app
-from models import Address, Item, Order, OrderItem, Picture, User
+from models import Address, Item, Order, OrderItem, Picture, User, Favorite
 from peewee import SqliteDatabase
 
-TABLES = [Address, Item, Order, OrderItem, Picture, User]
+TABLES = [Address, Item, Order, OrderItem, Picture, User, Favorite]
 
 
 class TestCase:
