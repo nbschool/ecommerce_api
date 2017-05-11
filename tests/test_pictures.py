100c7da1a5147820849bb98ea04c51edfd3fde98
Alessandro Brugioni <info@toysroom.it>
2017-05-11 11:59:13 +0200

    update tests after category field adding

--- tests/test_pictures.py
+++ tests/test_pictures.py
@@ -22,6 +22,7 @@ TEST_ITEM = {
     'price': 20.20,
     'description': 'svariati mariii',
     'availability': 1,
+    'category': 'scarpe',
 }
 
 TEST_ITEM2 = {
@@ -30,6 +31,7 @@ TEST_ITEM2 = {
     'price': 30.20,
     'description': 'svariati GINIIIII',
     'availability': 1,
+    'category': 'accessori',
 }
 
 TEST_PICTURE = {
