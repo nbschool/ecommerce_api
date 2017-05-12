f3c7f42b6747bc6b735a90cfccfaefa5b50e9f1c
German Lugo <bitnibs@gmail.com>
2017-05-12 11:51:22 +0200

    Add test_get_favorites_passs2__wrong.

--- tests/test_favorites.py
+++ tests/test_favorites.py
@@ -10,6 +10,7 @@ PASS2 = '0J9'
 # main endpoint for API
 API_ENDPOINT = '/{}'
 
+
 class TestFavorites(TestCase):
 
     def test_get_favorites__empty(self):
@@ -17,7 +18,7 @@ class TestFavorites(TestCase):
         user_path = 'favorites/'
         EXPECTED_MSG = b'[{"Message": "Sorry, You haven\'t select any favorite item yet."}]\n'
         resp = open_with_auth(self.app, API_ENDPOINT.format(user_path), 'GET',
-                      user.email, PASS1, None, None)
+                              user.email, PASS1, None, None)
         assert resp.status_code == NOT_FOUND
         assert resp.data == EXPECTED_MSG
 
@@ -25,9 +26,9 @@ class TestFavorites(TestCase):
         user = add_user(USER1, PASS1)
         item = add_item()
         favorite = add_favorite(user, item)
-        user_path = 'favorites/'       
+        user_path = 'favorites/'
         resp = open_with_auth(self.app, API_ENDPOINT.format(user_path), 'GET',
-                      user.email, PASS1, None, None)
+                              user.email, PASS1, None, None)
         data = json.loads(resp.data)
         assert resp.status_code == OK
         assert data[0]['user_id'] == str(user.id)
@@ -38,5 +39,14 @@ class TestFavorites(TestCase):
         user = add_user(USER1, PASS1)
         user_path = 'favorites/'
         resp = open_with_auth(self.app, API_ENDPOINT.format(user_path), 'GET',
-                      user.email, PASS2, None, None)
+                              user.email, PASS2, None, None)
+        assert resp.status_code == UNAUTHORIZED
+
+    def test_get_favorites_pass2__wrong(self):
+        """Forced case where a users uses the password of another user."""
+        user1 = add_user(USER1, PASS1)
+        user2 = add_user(None, PASS2)  # noqa: F841
+        user_path = 'favorites/'
+        resp = open_with_auth(self.app, API_ENDPOINT.format(user_path), 'GET',
+                              user1.email, PASS2, None, None)
         assert resp.status_code == UNAUTHORIZED
