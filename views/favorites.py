9b4ff204ab86a1f95f06b4583c348ba8018feb0a
German Lugo <bitnibs@gmail.com>
2017-05-12 15:34:04 +0200

    Rethink GET method of favorites (WIP mode on).

--- views/favorites.py
+++ views/favorites.py
@@ -11,34 +11,28 @@ class FavoritesHandler(Resource):
     """TEST DOCSTRING"""
     @auth.login_required
     def get(self):
-        """TODO Set the user status when logged"""
         user = g.user
         favorites = user.favorites
-        message_404 = "Sorry, You haven't select any favorite item yet." 
-        if favorites:
-            return favorites, OK
-        return to_json(message_404), NOT_FOUND
-
         result = []
-        for favorite in user.favorites:
+        for favorite in favorites:
             result.append(favorite.json())
         return result, OK
 
-    @auth.login_required
-    def post(self):
-        res = request.get_json()
+    # @auth.login_required
+    # def post(self):
+    #     res = request.get_json()
 
-        check_required_fields(
-            request_data=res,
-            required_fields=['item_uuid', 'user_uuid'],
-        )
+    #     check_required_fields(
+    #         request_data=res,
+    #         required_fields=['item_uuid', 'user_uuid'],
+    #     )
 
-        fav = Favorite.add_favorite(self, res)
-        # import pdb; set_trace()
-        new_fav = Favorite.create(
-            uuid=uuid.uuid4(),
-            item_id=fav['item_id'],
-            user_id=fav['user_id'],
-        )
+    #     fav = Favorite.add_favorite(self, res)
+    #     # import pdb; set_trace()
+    #     new_fav = Favorite.create(
+    #         uuid=uuid.uuid4(),
+    #         item_id=fav['item_id'],
+    #         user_id=fav['user_id'],
+    #     )
 
-        return new_fav.json(), CREATED
+    #     return new_fav.json(), CREATED
