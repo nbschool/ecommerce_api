d50ce084550c160d40317a65760b8239bcea9692
Simone Motta <simonemotta1986@gmail.com>
2017-05-12 12:24:01 +0200

    extract demo-content table creation

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,8 +1,3 @@
-"""
-Drop any test database tables (user, item, order, orderitem)
-and supply a new one db with new down-to-earth data.
-"""
-
 from peewee import SqliteDatabase
 from faker import Factory
 from colorama import init, Fore, Style
@@ -21,30 +16,32 @@ fake.seed(SEED)
 random.seed(SEED)
 
 
-TEXT_DISPLAY = Fore.MAGENTA + Style.BRIGHT + """
-                      WELCOME TO DEMO CONTENT CREATOR.
-                      --------------------------------
-                    """ + Fore.WHITE + Style.DIM + """
-                Here you could create a new simulated database.
-                """
+def get_random_row(table):
+    total_rows = table.select().count()
+    lucky_row = random.randint(1, total_rows)
+    return table.select().where(table.id == lucky_row).get()
 
 
-def write_db(num_items, num_users, num_orders, num_addrs):
-    """
-    Given the SEED 9623954 the first user email is
-    'fatima.caputo@tiscali.it', and its password is '9J0.'
-    """
-    user_creator(num_users)
-    address_creator(num_addrs)
-    item_creator(num_items)
-    order_creator(num_orders)
-    order_item_creator(random.randint(1, 7))
+def count_rows(table):
+    return table.select().count()
 
 
-def get_databases():
-    """create a list with the name of each .db file from main folder."""
-    list_of_db = glob.glob('*.db')
-    return list_of_db
+def set_db(database):
+    Order._meta.database = database
+    Item._meta.database = database
+    OrderItem._meta.database = database
+    User._meta.database = database
+    Address._meta.database = database
+
+
+def create_db(num_items, num_users, num_orders, num_addrs):
+    db = SqliteDatabase('database.db', autocommit=True)
+    if db.is_closed():
+        db.connect()
+    set_db(db)
+    create_tables()
+    write_db(num_items, num_users, num_orders, num_addrs)
+    good_bye('created')
 
 
 def user_creator(num_user):
@@ -87,6 +84,7 @@ def address_creator(num_addr):
                       'Greece', 'Italy', 'Portugal', 'Spain']
     for i in range(0, num_addr):
         country = random.choice(LIST_COUNTRIES)
+        user_id = count_rows(User)
         Address.create(
             uuid=fake.uuid4(),
             user_id=random.randint(1, user_id),
@@ -100,7 +98,9 @@ def address_creator(num_addr):
 
 def order_creator(num_order):
     for i in range(0, num_order):
+        user_id = count_rows(User)
         order_id = fake.uuid4()
+        address = count_rows(Address)
         Order.create(
             order_id=order_id,
             user_id=random.randint(1, user_id),
@@ -114,28 +114,65 @@ def order_item_creator(num_items):
     orders = Order.select()
     for order in orders:
         for e in range(1, num_items):
+            an_item = get_random_row(Item)
             quantity = random.randint(1, 5)
             order.add_item(an_item, quantity)
 
 
+def create_db(num_items, num_users, num_orders, num_addrs):
+    db = SqliteDatabase('database.db', autocommit=True)
+    if db.is_closed():
+        db.connect()
+    set_db(db)
+    create_tables()
+    good_bye('created')
+
+
+def get_databases():
+    """create a list with the name of each .db file from main folder."""
+    list_of_db = glob.glob('*.db')
+    return list_of_db
+
+
+def drops_all_tables(database):
+    """Doesn't drop unknown tables."""
+    tables = database.get_tables()
+    for table in tables:
+        if table == 'item':
+            Item.drop_table()
+        if table == 'order':
+            Order.drop_table()
+        if table == 'user':
+            User.drop_table()
+        if table == 'orderitem':
+            OrderItem.drop_table()
+        if table == 'address':
+            Address.drop_table()
+
+
+def create_tables():
+    User.create_table(fail_silently=True)
+    Item.create_table(fail_silently=True)
+    Order.create_table(fail_silently=True)
+    OrderItem.create_table(fail_silently=True)
+    Address.create_table(fail_silently=True)
+
+
 def good_bye(word, default='has'):
-    print(Fore.BLUE + Style.BRIGHT + '*-* Your database {1} been {0}. *-*'.format(word, default))
+    print(Fore.BLUE + Style.BRIGHT +
+          '*-* Your database {1} been {0}. *-*'.format(word, default))
     print(Fore.CYAN + Style.BRIGHT + '*_* Have a nice day! *_*')
     sys.exit()
 
 
-def prompt_menu_1(actions):
-    print(Fore.GREEN + Style.BRIGHT + '\t' * 2 + '*' * 47)
-    print(Fore.GREEN + Style.BRIGHT + '\t' * 2 + '* Press:' + ' ' * 37 + ' *')
-    for action in actions.values():
-        print(Fore.GREEN + Style.BRIGHT + '\t' * 2 + '  ({key}) {text}'.format(**action))
-    print(Fore.GREEN + Style.BRIGHT + '\t' * 2 + '*' * 47)
-
-    choice = None
-    while choice not in actions.keys():
-        choice = input(Fore.YELLOW + Style.BRIGHT + ' > ').strip()
-
-    actions[choice]['action']()
+def overwrite_db(num_items, num_users, num_orders, num_addrs):
+    db = SqliteDatabase('database.db', autocommit=False)
+    if db.is_closed():
+        db.connect()
+    set_db(db)
+    drops_all_tables(db)
+    create_tables()
+    good_bye('deleted')
 
 
 def main():
@@ -150,29 +187,13 @@ def main():
     parser.add_argument(
         '-o', '--orders', help='Set up the number of insertions in Order table.', default=10)
 
-
     args = parser.parse_args()
     num_users = args.users
     num_addrs = args.addresses
     num_items = args.items
     num_orders = args.orders
 
-    OVERWRITE_ACTIONS = {
-        '1': {
-            'key': '1', 'text': 'Add data to the current database',
-            'action': lambda: write_db(num_items, num_users, num_orders, num_addrs)
-        },
-        '': {
-            'key': 'Enter', 'text': 'Just exit',
-            'action': lambda: good_bye('change', default='hasn\'t')
-        },
-    }
-
-    print(TEXT_DISPLAY)
-    list_db = get_databases()
-    if len(list_db) != 0:
-        print(Fore.YELLOW + Style.BRIGHT + 'You have already a database.')
-        prompt_menu_1(OVERWRITE_ACTIONS)
+    OVERWRITE_ACTIONS = {overwrite_db(num_items, num_users, num_orders, num_addrs)}
 
 
 if __name__ == '__main__':
