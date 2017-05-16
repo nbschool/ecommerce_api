1aff46044b4f87fdb5405a5a1dcc44103b4c0171
German Lugo <bitnibs@gmail.com>
2017-05-16 13:51:30 +0200

    Fix POST method of Favorites.

--- views/favorites.py
+++ views/favorites.py
@@ -1,7 +1,7 @@
 from auth import auth
 from flask import g, request
 from flask_restful import Resource
-from models import Favorite
+from models import Favorite, Item
 from utils import check_required_fields, to_json
 from http.client import (CREATED, NOT_FOUND, OK, BAD_REQUEST)
 import uuid
@@ -18,21 +18,24 @@ class FavoritesHandler(Resource):
             result.append(favorite.json())
         return result, OK
 
-    # @auth.login_required
-    # def post(self):
-    #     res = request.get_json()
+    @auth.login_required
+    def post(self):
+        user = g.user
+        item_uuid = request.json[0]['item_uuid']
+        check_required_fields(
+            request_data=request.get_json(),
+            required_fields=('item_uuid', 'user_uuid'),
+        )
 
-    #     check_required_fields(
-    #         request_data=res,
-    #         required_fields=['item_uuid', 'user_uuid'],
-    #     )
+        item = Item.select().where(Item.uuid == item_uuid).get()
+        has_already = Item.is_favorite(self, user, item)
 
-    #     fav = Favorite.add_favorite(self, res)
-    #     # import pdb; set_trace()
-    #     new_fav = Favorite.create(
-    #         uuid=uuid.uuid4(),
-    #         item_id=fav['item_id'],
-    #         user_id=fav['user_id'],
-    #     )
+        if has_already:
+            return { "message" : "ALREADY_INSERTED" }.json() , OK
 
-    #     return new_fav.json(), CREATED
+        favorite = Favorite.create(
+                uuid=uuid.uuid4(),
+                item=item,
+                user=user,
+        )
+        return favorite.json(), CREATED
