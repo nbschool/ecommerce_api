e87d762bfad4367114600c71d5642b46457c99db
German Lugo <bitnibs@gmail.com>
2017-05-16 14:32:03 +0200

    Add checking when item does not exist.

--- views/favorites.py
+++ views/favorites.py
@@ -27,11 +27,15 @@ class FavoritesHandler(Resource):
             required_fields=('item_uuid', 'user_uuid'),
         )
 
-        item = Item.select().where(Item.uuid == item_uuid).get()
+        try:
+            item = Item.select().where(Item.uuid == item_uuid).get()
+        except:
+            return {"message" : "ITEM DOESN'T EXIST"}, OK
+            
         has_already = Item.is_favorite(self, user, item)
 
         if has_already:
-            return {"message" : "ALREADY_INSERTED"}, OK
+            return {"message" : "ALREADY INSERTED"}, OK
 
         favorite = Favorite.create(
                 uuid=uuid.uuid4(),
