67ef093985dd053dcba2182151f657a73574c276
German Lugo <bitnibs@gmail.com>
2017-05-18 10:10:52 +0200

    Add util functions.

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
-from flask import Response
+from flask import Response, abort
+from http.client import BAD_REQUEST
 
 IMAGE_FOLDER = 'images'
 
@@ -53,3 +54,24 @@ def generate_response(data, status, mimetype='application/vnd.api+json'):
         status=status,
         mimetype=mimetype
     )
+
+
+def check_required_fields(required_fields, request_data):
+    """
+    Check whether request_data provides all fields in
+    required_fields and they are not empty, bad request
+    is raised otherwise
+    """
+    for field in required_fields:
+        try:
+            value = request_data[field]
+            non_empty_str(value, field)
+        except (KeyError, ValueError):
+            abort(BAD_REQUEST)
+
+
+def non_empty_str(val, name):
+    """ Custom type for reqparser, blocking empty strings """
+    if not str(val).strip():
+        raise ValueError('The argument {} cannot be empty'.format(name))
+    return str(val)
