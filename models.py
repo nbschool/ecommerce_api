540fc7f5b5833d864f43a8c82c3b700ca4442af0
German Lugo <bitnibs@gmail.com>
2017-05-19 15:36:56 +0200

    Refactor last method.

--- models.py
+++ models.py
@@ -166,7 +166,7 @@ class User(BaseModel):
 
     def add_favorite(self, item):
         """Link the favorite item to user."""
-        Favorite.create(
+        return Favorite.create(
                 uuid=uuid4(),
                 item=item,
                 user=self,
