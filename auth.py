f4daa1c25d0eab29ac1f985808e529c6d864b329
Daniele Ding <dinghino@gmail.com>
2017-05-24 13:32:36 +0200

    update docstrings on tests and auth

--- auth.py
+++ auth.py
@@ -1,7 +1,8 @@
-"""Auth module used to handle authorization requests and checks"""
+"""
+Auth module handles authorization requests and checks.
+"""
 
 from flask_login import login_required, LoginManager
-
 from models import User
 
 login_manager = LoginManager()
@@ -25,6 +26,7 @@ class Auth:
         We forward the flask_login current_user converted from a proxy object
         to our ``models.User``. We import the flask_login current_user inside
         the method to forbid a direct import from flask_login.
+
         Returns:
             models.User: currently logged user
         """
@@ -35,9 +37,10 @@ class Auth:
     def init_app(app):
         """
         Wrapper for the flask_login method ``LoginManager::init_app``.
+
         Args:
             Flask app (Flask): The Flask app to initialize with the
-            login manager
+                login manager
         """
         return login_manager.init_app(app)
 
@@ -49,8 +52,10 @@ auth = Auth()
 def load_user(user_id):
     """
     Current user loading logic. If the user exists return it, otherwise None.
+
     Args:
         user id (int): Peewee user id
+
     Returns:
         models.User: The requested user
     """
@@ -65,8 +70,10 @@ def load_user_from_request(request):
     """
     User login validation logic. If the user exists with the given username
     and with the correct password then returns the user, otherwise None.
+
     Args:
         request (Request): The flask request object used in endpoint handlers
+
     Returns:
         models.User: The logged user, None if login fails
     """
