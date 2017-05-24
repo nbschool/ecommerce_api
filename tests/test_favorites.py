2c9c5aa36b7cbec638c0db32bc4e63afcb7c7b6b
German Lugo <bitnibs@gmail.com>
2017-05-24 12:36:02 +0200

    Adjust test to schemas.

--- tests/test_favorites.py
+++ tests/test_favorites.py
@@ -1,6 +1,8 @@
-from tests.test_utils import RESULTS, add_user, open_with_auth, add_favorite, add_item, assert_valid_response
+from tests.test_utils import (RESULTS, add_user, open_with_auth, add_favorite, add_item,
+                              assert_valid_response, json_favorite, format_jsonapi_request)
 from tests.test_case import TestCase
-from http.client import OK, UNAUTHORIZED, CREATED, NOT_FOUND
+from http.client import OK, UNAUTHORIZED, CREATED, NOT_FOUND, BAD_REQUEST
+from models import Favorite, Item
 import json
 
 USER1 = 'fatima.caputo@tiscali.it'
@@ -44,7 +46,7 @@ class TestFavorites(TestCase):
         favorite3 = add_favorite(user, item3)
         user_path = 'favorites/'
         resp = open_with_auth(self.app, API_ENDPOINT.format(user_path), 'GET',
-                              user.email, PASS1, None, None)
+                              user.email, PASS1, None, None)      
         assert resp.status_code == OK
         expected_result = EXPECTED_RESULTS['get_favorites2__success']
         assert_valid_response(resp.data, expected_result)
@@ -74,29 +76,51 @@ class TestFavorites(TestCase):
 
     def test_post_favorites__fail(self):
         user = add_user(USER1, PASS1)
-        data = {"item_uuid": "3", "user_uuid": str(user.uuid)}
+        data = {
+                "data": {
+                    "type": "favorite",
+                    "attributes": {
+                        "item_uuid": "2aabf825-40b3-03d5-e686-9eaebd156c0e"
+                    }
+                }
+        }
         user_path = 'favorites/'
         resp = open_with_auth(self.app, API_ENDPOINT.format(user_path), 'POST',
                               user.email, PASS1, 'application/json',
                               json.dumps(data))
         assert resp.status_code == OK
-        assert resp.data == b'{"message": "Item 3 doesn\'t exist."}\n'
+        assert Favorite.select().count() == 0
+        expected_result = EXPECTED_RESULTS['post_favorites__fail']
+        assert_valid_response(resp.data, expected_result)
 
-    def test_post_favorites_success(self):
+    def test_post_favorites2__fail(self):
         user = add_user(USER1, PASS1)
-        item = add_item()
-        data = {"item_uuid": str(item.uuid),
-                "user_uuid": str(user.uuid),
+        data = {
+                "data": {
+                    "type": "favorite",
+                    "attributes": {
+                        "item_uuid": ""
+                    }
                 }
+        }
         user_path = 'favorites/'
         resp = open_with_auth(self.app, API_ENDPOINT.format(user_path), 'POST',
                               user.email, PASS1, 'application/json',
                               json.dumps(data))
-        # import pdb; pdb.set_trace()
-        assert resp.status_code == CREATED
-        expected_result = EXPECTED_RESULTS['post_favorites__success']
-        assert_valid_response(resp.data, expected_result)
+        assert resp.status_code == BAD_REQUEST
+        assert Favorite.select().count() == 0
 
+    def test_post_favorites__success(self):
+        user = add_user(USER1, PASS1)
+        item = add_item()
+        favorite = json_favorite(str(item.uuid))
+        data = format_jsonapi_request('favorite', favorite)
+        user_path = 'favorites/'
+        resp = open_with_auth(self.app, API_ENDPOINT.format(user_path), 'POST',
+                              user.email, PASS1, 'application/json',
+                              json.dumps(data))
+        assert resp.status_code == CREATED
+        assert Favorite.select().count() == 1
 
     def test_delete_favorites__success(self):
         user = add_user(USER1, PASS1)
@@ -105,9 +129,9 @@ class TestFavorites(TestCase):
         user_path = 'favorites/{}'.format(str(item.uuid))
         resp = open_with_auth(self.app, API_ENDPOINT.format(user_path), 'DELETE',
                               user.email, PASS1, None, None)
-
         assert resp.status_code == OK
         assert len(resp.data) == 67
+        assert Favorite.select().count() == 0
 
     def test_delete_favorites__fail(self):
         user = add_user(USER1, PASS1)
@@ -124,8 +148,10 @@ class TestFavorites(TestCase):
         user2 = add_user(USER2, PASS2)
         item = add_item()
         add_favorite(user2, item)
+
         user_path = 'favorites/{}'.format(str(item.uuid))
         resp = open_with_auth(self.app, API_ENDPOINT.format(user_path), 'DELETE',
                               user1.email, PASS1, None, None)
 
         assert resp.status_code == NOT_FOUND
+        assert Favorite.select().count() == 1
